e01a0a79d272344e78e63956217233911715711e
alwaysblue930 b <alwaysblue930@gmail.com>
2024-11-01 11:36:23 +0000

    feat: add basic structure for conditional

--- element.sh
+++ element.sh
@@ -1,1 +1,19 @@
-echo "Please provide an element as an argument."
+
+ARG=$1
+PSQL = "psql -X --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [[ -z $ARG ]]
+then
+  echo "Please provide an element as an argument."
+else
+  if [[ $ARG =~ ^[0-9]+$ ]]
+  then
+    echo "look for id"
+  elif [[ ${#ARG} -le 2 ]]
+  then
+    echo "look for symbol"
+  else
+    echo "look for name"
+  fi
+
+fi
