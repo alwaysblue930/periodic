d4751d271ff41073e7903cb7b442fefae7b24d73
alwaysblue930 b <alwaysblue930@gmail.com>
2024-11-01 12:02:34 +0000

    feat: add query and fetching

--- element.sh
+++ element.sh
@@ -1,19 +1,29 @@
 
 ARG=$1
-PSQL = "psql -X --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 if [[ -z $ARG ]]
 then
   echo "Please provide an element as an argument."
 else
+  PREFIX_QUERY="SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM properties 
+  INNER JOIN elements USING(atomic_number) INNER JOIN types USING(type_id)"
+  CONDITIONAL=""
   if [[ $ARG =~ ^[0-9]+$ ]]
   then
-    echo "look for id"
+    CONDITIONAL="WHERE atomic_number=$ARG;"
   elif [[ ${#ARG} -le 2 ]]
   then
+    CONDITIONAL="WHERE symbol='$ARG';"
     echo "look for symbol"
   else
+    CONDITIONAL="WHERE name='$ARG';"
     echo "look for name"
   fi
+  RESULT=$($PSQL "$PREFIX_QUERY $CONDITIONAL")
+  echo "$RESULT" | while IFS='|' read -r ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT
+  do
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+  done
 
 fi
