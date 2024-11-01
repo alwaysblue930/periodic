afa10fdd5e4150126006385f3c00c5b83151ea83
alwaysblue930 b <alwaysblue930@gmail.com>
2024-11-01 13:16:20 +0000

    feat: add db last two problems

--- element.sh
+++ element.sh
@@ -15,19 +15,15 @@ else
   elif [[ ${#ARG} -le 2 ]]
   then
     CONDITIONAL="WHERE symbol='$ARG';"
-    echo "look for symbol"
   else
     CONDITIONAL="WHERE name='$ARG';"
-    echo "look for name"
   fi
   RESULT=$($PSQL "$PREFIX_QUERY $CONDITIONAL")
   if [[ -z $RESULT ]]
   then
     echo "I could not find that element in the database."
   else
-    echo "$RESULT" | while IFS='|' read -r ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT
-    do
-      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
-    done
+    echo "$RESULT" | IFS='|' read ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MPC BPC
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MPC celsius and a boiling point of $BPC celsius."
   fi
 fi
